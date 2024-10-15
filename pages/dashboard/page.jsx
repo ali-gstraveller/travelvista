e6e6cb9d8355af780ac4c34ae373b3d438ea04e9
ali-gstraveller <aliajazgstravellers@gmail.com>
2024-10-15 10:01:05 +0500

    added google auth

---
+++ pages/dashboard/page.jsx
@@ -0,0 +1,32 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+import styles from "../../styles/Home.module.css";
+
+export default function Dashboard() {
+    const { data: session } = useSession();
+
+    return (<div>
+        <h4 style={{border:"1px solid black" ,width:'90px',padding:"2px"
+         }} > Travel Vista </h4>
+        {!session ? (    
+        <>  
+        <button style={{ marginLeft: '1150px' }} onClick={() => signIn()}>Sign in</button>
+<h1 style={{ textAlign: 'center' }} > Welcome, Kindly Login to Continue </h1>
+        </>
+        ) : (
+        <>
+            <button style={{ marginLeft: '1150px' }} onClick={() => signOut()}>Sign out</button>
+            <h1 style={{ textAlign: 'center' }} > Welcome, {session.user.name} !  </h1>
+        </>
+        )}
+        <div style={{ backgroundImage:  'url("/cover.webp")',width:'1250px',height:"700px" }} >
+            {/* <h1 style={{color:"white", marginLeft:"450px" }} >Kindly enter your preference !</h1> */}
+            <input placeholder="where to ?" type="text" style={{ borderRadius:"15px", padding:'9px',marginLeft:"550px" ,marginTop:"50px"   }} />
+            <button style={{ borderRadius:"15px", padding:'9px'}}  > Submit </button>
+
+        </div>
+
+
+    </div>
+    )
+}
