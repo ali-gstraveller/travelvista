3d1e3c782fb010de2f9599d60bdb6879a5511f2c
ali-gstraveller <aliajazgstravellers@gmail.com>
2024-11-02 10:45:55 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Dashboard from "./dashboard/page";
-
+import { useMediaQuery } from 'react-responsive';
 
 export default function Home() {
   
   const { data: session } = useSession() ;
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
 
   return (
     <div className={styles.container}>
@@ -18,13 +20,25 @@ export default function Home() {
       </Head>
         <main className={styles.main}>
         {/* <Dashboard/> */}
+        
+        {isMobile ? (
         <Image
-      src="/GS.png"
-      width={1250}
-      height={850}
-      alt="Picture of the author"
-      onClick={  ()=>{   window.location.href="https://www.trip.com/flights/?to=home&Allianceid=5546479&SID=124875388&trip_sub1=gstravellers&trip_sub3=D656182"   }    }
-    />
+        src="/mobile-GS.png"
+        width={392}
+        height={848}
+        alt="Picture of the author"
+        onClick={  ()=>{   window.location.href="https://www.trip.com/flights/?to=home&Allianceid=5546479&SID=124875388&trip_sub1=gstravellers&trip_sub3=D656182"   }    }
+      />
+      ) : (
+        <Image
+        src="/GS.png"
+        width={1250}
+        height={850}
+        alt="Picture of the author"
+        onClick={  ()=>{   window.location.href="https://www.trip.com/flights/?to=home&Allianceid=5546479&SID=124875388&trip_sub1=gstravellers&trip_sub3=D656182"   }    }
+      />  
+      )}
+      
         </main>
       
     </div>
